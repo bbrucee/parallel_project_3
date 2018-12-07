39e053ec5b820606ba019834202c89f628275d1b
Brendan <abae97@gmail.com>
2018-12-06 22:00:21 -0500

    CUDA now has all functions, needs testing confirmation and then timing for both CUDA/CPU

--- problem1/vectorstatsCUDA.cpp
+++ problem1/vectorstatsCUDA.cpp
@@ -9,7 +9,10 @@
 // 2. Half of those threads then compares itself to the thread on the other half of the the half and sets the max of the two
 // 3. Repeat 
 // 4. Maximum element is now in the 0th index and is written
-__global__ void find_maxKernel(double* input_array, int array_max)
+// This is in general a reduction and a similar pseduocode is used for mean/min/std calculations
+
+
+__global__ void find_maxKernel(double* input_array, double array_max)
 {
 	extern __shared__ double maximum[];
 	// Each thread loads one element from global to shared mem
@@ -31,15 +34,15 @@ __global__ void find_maxKernel(double* input_array, int array_max)
 	if (tid==0) array_max = maximum[0];
 }
 
-extern void find_max(double* input_array, int input_size)
+extern double find_max(double* input_array, int input_size)
 {
-	int max_value = 0;
+	double max_value = 0;
     find_maxKernel<<<1, input_size>>>(input_array, max_value);
     cudaDeviceSynchronize();
     return max_value;
 }
 
-__global__ void find_minKernel(double* input_array, int array_min)
+__global__ void find_minKernel(double* input_array, double array_min)
 {
 	extern __shared__ double minimum[];
 	// Each thread loads one element from global to shared mem
@@ -61,12 +64,76 @@ __global__ void find_minKernel(double* input_array, int array_min)
 	if (tid==0) array_min = minimum[0];
 }
 
-extern void find_min(double* input_array, int input_size)
+extern double find_min(double* input_array, int input_size)
 {
-	int min_value = 0;
+	double min_value = 0;
     find_minKernel<<<1, input_size>>>(input_array, min_value);
     cudaDeviceSynchronize();
     return min_value;
+}
+
+__global__ void find_sumKernel(double* input_array, double out_sum)
+{
+	extern __shared__ double array_sum[];
+	// Each thread loads one element from global to shared mem
+    int i = blockIdx.x*blockDim.x + threadIdx.x;
+    int tid =  threadIdx.x;
+    array_sum[tid] = input_array[i];
+    __syncthreads;
+
+
+   	for(int s=1; s<blockDim.x; s*=2){
+   		int index = 2*s*tid;
+   		if(index < blockDim.x){
+			array_sum[index] += array_sum[index+s];
+   		}
+   		__syncthreads;
+   	}
+
+	if (tid==0) out_sum = array_sum[0];
+}
+
+extern double find_mean(double* input_array, int input_size)
+{
+	double mean_value = 0;
+    find_sumKernel<<<1, input_size>>>(input_array, mean_value);
+    cudaDeviceSynchronize();
+    return mean_value/input_size;
+}
+
+
+__global__ void find_squaresumKernel(double* input_array, double out_sum)
+{
+	extern __shared__ double array_sum[];
+	// Each thread loads one element from global to shared mem
+    int i = blockIdx.x*blockDim.x + threadIdx.x;
+    int tid =  threadIdx.x;
+    array_sum[tid] = input_array[i]*input_array[i];
+    __syncthreads;
+
+
+   	for(int s=1; s<blockDim.x; s*=2){
+   		int index = 2*s*tid;
+   		if(index < blockDim.x){
+			array_sum[index] += array_sum[index+s]*array_sum[index+s];
+   		}
+   		__syncthreads;
+   	}
+
+	if (tid==0) out_sum = array_sum[0];
+}
+
+
+extern double find_std(double* input_array, int input_size)
+{
+	double mean_value = 0;
+	double squaresum_value = 0;
+    find_sumKernel<<<1, input_size>>>(input_array, mean_value);
+ 	find_squaresumKernel<<1, input_size>>(input_array, squaresum_value);
+    cudaDeviceSynchronize();
+    mean_value = mean_value/input_size;
+    squaresum_value = squaresum_value/input_size;
+    return sqrt(squaresum_value - mean_value*mean_value);
 }
 
 
