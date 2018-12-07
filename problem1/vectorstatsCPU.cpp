5cfee314b4ca8a32a7037cee237e80066a4ee6b8
Brendan <abae97@gmail.com>
2018-12-06 19:04:41 -0500

    made a folder

--- problem1/vectorstatsCPU.cpp
+++ problem1/vectorstatsCPU.cpp
@@ -3,31 +3,36 @@
 #include <limits>
 
 
-float find_max(float* input_array, int input_size)
+double find_max(double* input_array, int input_size)
 {
-    if(input_size == 0){return 0;}
-    float max_value = std::numeric_limits<float>::min();
+    if(input_size == 0)
+        return 0;
+    double max_value = std::numeric_limits<double>::min();
     for(int i = 0; i<input_size; i++){
-        if(input_array[i] > max_value){max_value = input_array[idx]}
+        if(input_array[i] > max_value)
+            max_value = input_array[i];
     }
     return max_value;
 }
     
-float find_min(float* input_array, int input_size)
+double find_min(double* input_array, int input_size)
 {
-    if(input_size == 0){return 0;}
-    float min_value = std::numeric_limits<float>::max();
+    if(input_size == 0)
+        return 0;
+    double min_value = std::numeric_limits<double>::max();
     for(int i = 0; i<input_size; i++){
-        if(input_array[i] < min_value){min_value = input_array[idx]}
+        if(input_array[i] < min_value)
+            min_value = input_array[i];
     }
     return min_value;
 
 }
 
-float find_mean(float* input_array, int input_size)
+double find_mean(double* input_array, int input_size)
 {
-    if(input_size == 0){return 0;}
-    float mean_value = 0;
+    if(input_size == 0)
+        return 0;
+    double mean_value = 0;
     for(int i = 0; i<input_size; i++){
         mean_value += input_array[i];
     }
@@ -35,11 +40,12 @@ float find_mean(float* input_array, int input_size)
     return mean_value;
 }
     
-float find_std(float* input_array, int input_size)
+double find_std(double* input_array, int input_size)
 {
-    if(input_size == 0){return 0;}
-    float mean_value = 0;
-    float square_sum = 0;
+    if(input_size == 0)
+        return 0;
+    double mean_value = 0;
+    double square_sum = 0;
     for(int i = 0; i<input_size; i++){
         mean_value += input_array[i];
         square_sum += input_array[i]*input_array[i];
@@ -52,7 +58,7 @@ float find_std(float* input_array, int input_size)
 int vectorstatsCPUtest()
 {
     // Test function compares our function outputs to values computed using numpy externally
-    float array[6] = [1.2342, 2.232421, 1.214124, 4.3252, 5.12314, 2.343241];
+    double array[6] = {1.2342, 2.232421, 1.214124, 4.3252, 5.12314, 2.343241};
     int size = 6;
     printf("Homebrew max is %f\n", find_max(array, size));
     printf("Homebrew min is %f\n", find_min(array, size));
