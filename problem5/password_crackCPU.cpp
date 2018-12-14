fe58c08af38bcd06ecc50a6e9a961a127fb039b0
Brendan <abae97@gmail.com>
2018-12-14 17:20:14 -0500

    CPU speed testing

--- problem5/password_crackCPU.cpp
+++ problem5/password_crackCPU.cpp
@@ -12,6 +12,8 @@
 #include <pthread.h>
 #include <unistd.h>
 
+#include "password_crack.h"
+
 using namespace std;
 
 char map(int convert){
@@ -72,29 +74,33 @@ int original_main() {
     return 0;
 }
 
-int speedtest(char* input_string) {
-  char passwordStr[] = input_string;
-
-  int possibleLen = strlen(passwordStr);
+void* crack(void* args){
+  // Get arguments
+  struct params *params = (struct params*) args;
+  size_t password = params->password;
+  int possLen = params->passLen;
+  int totalThreads = params->totalThreads;
+  int currThread = params->currThread;
+  int count = 0;
 
   hash<string> ptr_hash;
-  size_t password = ptr_hash(string(passwordStr));    
-  printf("-Starting Non-Parallel Password Cracker-\n");
 
+  //printf("Values: %d\t %s\n", currLen, password);
+  for(int currLen = 1; currLen <= possLen; ++currLen) {
+    char* guess = new char[currLen+1];
+    memset(guess, '\0', currLen+1);
 
-  struct timespec start, finish;
-  double elapsed;
+    int partitionOfPass = pow(setSize, (float) currLen) / totalThreads;
+    int passStart = currThread * partitionOfPass;
 
-  // Start Timer
-  clock_gettime(CLOCK_MONOTONIC, &start);
+    //printf("Pass: %d\t Thread: %d\t Start: %d\t End: %d\n", currLen, currThread, passStart, passStart + partitionOfPass);
 
-  // Loop through len 1 - possible len
-  for (int currLen = 1; currLen <= possibleLen; ++currLen) {
-  // Loop for all possible combinations
-    char* guess = new char[currLen + 1];
-    memset(guess, '\0', currLen +1);
+    for (int currChar = passStart; currChar <= passStart + partitionOfPass; ++currChar) {
+      if (done == true) {
+        break;
+      }
+      count++;
 
-    for (int currChar = 0; currChar <= (pow(setSize, (float) currLen)); ++currChar) {
       // Set guess
       for (int guessIndex = 0; guessIndex < currLen; ++guessIndex) {
         char temp = map((currChar / (int) pow(setSize, guessIndex)) % (int) setSize);
@@ -103,25 +109,67 @@ int speedtest(char* input_string) {
       //printf("Iteration: %d\tGuess: %s\n", currChar, guess);
 
       // Check if it compares
-      if ( password == ptr_hash(string(guess))) {
-        printf("Match Found Single!! \nLen: %d\tGuess: %s\n",currLen, guess);
-        break;
+      if (password == ptr_hash(string(guess))) {
+
+        printf("Match Found Parallel!! \nLen: %d\tGuess: %s\t Iterations: %d\t Current Count: %d\n",currLen, guess, count, currChar);
+        done = true;
+        return (void*) guess;
       }
     }
   }
+   //printf("Thread: %d Finished! Iterations: %d\n", currThread, count);
+  return NULL;
+}
 
-  clock_gettime(CLOCK_MONOTONIC, &finish);
-  elapsed = (finish.tv_sec - start.tv_sec);
-  elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
-  printf("Time: %f\n", elapsed);
+int speedtest(char* input_string) {
+  char passwordStr[] = input_string;
 
-  return 0;
+  int possibleLen = strlen(passwordStr);
 
+  hash<string> ptr_hash;
+  size_t password = ptr_hash(string(passwordStr));    
+  printf("-Starting pthread Password Cracker-\n");
+  int numThreads = 4;
+
+  struct timespec start, finish;
+  double elapsed;
+
+  // Start Timer
+  clock_gettime(CLOCK_MONOTONIC, &start);
+
+  pthread_t thread[numThreads];
+
+    // Outer loop to check different lengths of passwords
+    for (int i = 0; i < numThreads; i++){
+      struct params* args = new struct params;
+      args->password = password;
+      args->passLen = possibleLen;
+      args->totalThreads = numThreads;
+      args->currThread = i;
+      pthread_create( &thread[i], NULL, crack, (void*) args);
+    }
+
+    //printf("All threads running\n");
+
+    for (int i = 0; i < numThreads; i++){
+      char* pass_guess;
+      pthread_join(thread[i], (void**) &pass_guess);
+      if(pass_guess != NULL){
+        printf("Returned Value: %s\n", pass_guess);
+      }
+    }
+
+    clock_gettime(CLOCK_MONOTONIC, &finish);
+    elapsed = (finish.tv_sec - start.tv_sec);
+    elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+    printf("Time: %f\n", elapsed);
+    
+    return 0;
 }
 
 int main()
 {
-  string passwords[10] = {"bv37qi#f", "racecar", "141njif", "alx9123n", "123dnar3", "password", "parall12", "a2312", "912341", "1332"};
+  char* passwords[] = {"bv37qi#f", "racecar", "141njif", "alx9123n", "123dnar3", "password", "parall12", "a2312", "912341", "1332"};
   for(int i=0; i<10; i++){
     speedtest(passwords[i]);
   }
