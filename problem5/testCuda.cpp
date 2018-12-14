748a89e4dd21b679255a758b02b8d1cdac5fda09
Ubuntu <ubuntu@ip-172-31-87-52.ec2.internal>
2018-12-14 02:10:58 +0000

    testing cuda stuff

--- problem5/testCuda.cpp
+++ problem5/testCuda.cpp
@@ -1,15 +1,24 @@
-__global__  void  AplusB( int  *ret,  int  a,  int  b) {
-    ret[threadIdx.x] = a + b + threadIdx.x;
+#include <iostream>
+#include <stdio.h>
+
+__global__  void  AplusB( char  *ret,  int  a,  int  b) {
+	if (threadIdx.x == 99) {
+		ret[threadIdx.x] = (char)36;
+	} else {
+		ret[threadIdx.x] = (char) (a + b + threadIdx.x);
+	}
 }
+
 int main() {
-    int *ret;
-    cudaMalloc(&ret, 1000 * sizeof(int));
-    AplusB<<< 1, 1000 >>>(ret, 10, 100);
-    int *host_ret = (int *)malloc(1000 * sizeof(int));
-    cudaMemcpy(host_ret, ret, 1000 * sizeof(int), cudaMemcpyDefault);
-    for(int i=0; i<1000; i++)
-        printf("%d: A+B = %d\n", i, host_ret[i]); 
-    free(host_ret);
+    char *ret;
+    cudaMallocManaged(&ret, 100 * sizeof(char));
+	
+	//<<<numBlocks, threadsPerBlock>>>
+    AplusB<<< 1, 100 >>>(ret, 10, 10);
+	cudaDeviceSynchronize();
+
+    for(int i=0; i<100; i++)
+        printf("%d: A+B = %c\n", i, ret[i]); 
     cudaFree(ret); 
     return  0;
 }
