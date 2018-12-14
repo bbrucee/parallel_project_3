2c079ae254963edd6834cb91c9076e83fc6bc6dd
Nick <nac542@gmail.com>
2018-12-13 21:20:49 -0500

    forgot to add stuff

--- problem5/testCuda.cpp
+++ problem5/testCuda.cpp
@@ -1,31 +1,38 @@
 #include <iostream>
 #include <stdio.h>
 
-__global__  void  AplusB( char  *ret,  int  a,  int  b) {
+__global__  void  AplusB( char  *ret,  int  a,  int  b, char* tempChar) {
   int index = threadIdx.x;
   char guess[] = "temp";
-	if (threadIdx.x == 99) {
-		ret[threadIdx.x] = (char)36;
-	} else {
-		ret[threadIdx.x] = (char) (a + b + threadIdx.x);
-	}
+  if (threadIdx.x == 99) {
+    ret[threadIdx.x] = (char)36;
+  } else {
+    ret[threadIdx.x] = (char) (a + b + threadIdx.x);
+  }
 }
 
 int main() {
-    char *ret;
-    cudaMallocManaged(&ret, 100 * sizeof(char));
-	
-	//<<<numBlocks, threadsPerBlock>>>
-    AplusB<<< 1, 100 >>>(ret, 10, 10);
-	cudaDeviceSynchronize();
+  char *ret, *tempChar;
 
-    for(int i=0; i<100; i++)
-        printf("%d: A+B = %c\n", i, ret[i]); 
-    cudaFree(ret); 
-    return  0;
+  cudaMallocManaged(&ret, 100 * sizeof(char));
+  cudaMallocManaged(&tempChar, 5 * sizeof(char));
+
+  printf("Check before: %s\n", tempChar);
+  
+  //<<<numBlocks, threadsPerBlock>>>
+  AplusB<<< 1, 100 >>>(ret, 10, 10, tempChar);
+  cudaDeviceSynchronize();
+
+  for(int i=0; i<100; i++)
+    printf("%d: A+B = %c\n", i, ret[i]); 
+  cudaFree(ret); 
+
+  printf("Stirng: %s\n", tempChar);
+  cudaFree(tempChar);
+  return  0;
 }
 
-
+/*
 int index = blockIdx.x * blockDim.x + threadIdx.x;
     //printf("Values: %d\t %s\n", currLen, password);
     int currLen = (int)(logf(index) / logf(*setSize)) + 1;
@@ -48,3 +55,4 @@ int index = blockIdx.x * blockDim.x + threadIdx.x;
     //printf("Thread: %d Finished! Iterations: %d\n", currThread, count);
     free(guess1);
   }
+  */
